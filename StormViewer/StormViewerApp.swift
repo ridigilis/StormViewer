74844f418f261b4bf4afde335854b4d6b8969011
ridigilis <rgroner2@gmail.com>
2023-12-07 16:27:49 -0600

    Initial Commit

---
+++ StormViewer/StormViewerApp.swift
@@ -0,0 +1,17 @@
+//
+//  StormViewerApp.swift
+//  StormViewer
+//
+//  Created by Ricky David Groner II on 12/7/23.
+//
+
+import SwiftUI
+
+@main
+struct StormViewerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
